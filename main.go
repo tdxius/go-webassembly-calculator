78cae46d2db1d7f95e4a0a9b22da12a4160454d7
Tadej <tadej.996@gmail.com>
2021-02-04 18:33:57 +0100

    Add add and sub functions.

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+  "syscall/js"
+)
+
+func add(this js.Value, i []js.Value) interface{} {
+  return js.ValueOf(i[0].Int() + i[1].Int())
+}
+
+func sub(this js.Value, i []js.Value) interface{} {
+  return js.ValueOf(i[0].Int() - i[1].Int())
+}
+
+func registerCallbacks() {
+  js.Global().Set("add", js.FuncOf(add))
+  js.Global().Set("sub", js.FuncOf(sub))
+}
+
 func main() {
-  println("Holla")
+  c := make(chan struct{}, 0)
+
+  println("Go WebAssembly Initialized")
+  registerCallbacks()
+
+  <-c
 }
