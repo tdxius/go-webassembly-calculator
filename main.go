339b6f36ca29547acce27fec92c019caac2066a0
Tadej <tadej.996@gmail.com>
2021-02-05 08:14:42 +0100

    Add inputs and output to HTML elements and add action buttons.

--- main.go
+++ main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+  "strconv"
   "syscall/js"
 )
 
 func add(this js.Value, i []js.Value) interface{} {
-  return js.ValueOf(i[0].Int() + i[1].Int())
+  value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+  value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+
+  int1, _ := strconv.Atoi(value1)
+  int2, _ := strconv.Atoi(value2)
+
+  js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1 + int2)
+
+  return true
 }
 
-func sub(this js.Value, i []js.Value) interface{} {
-  return js.ValueOf(i[0].Int() - i[1].Int())
+func subtract(this js.Value, i []js.Value) interface{} {
+  value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+  value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+
+  int1, _ := strconv.Atoi(value1)
+  int2, _ := strconv.Atoi(value2)
+
+  js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1 - int2)
+
+  return true
 }
 
 func registerCallbacks() {
   js.Global().Set("add", js.FuncOf(add))
-  js.Global().Set("sub", js.FuncOf(sub))
+  js.Global().Set("subtract", js.FuncOf(subtract))
 }
 
 func main() {
